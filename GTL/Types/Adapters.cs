aa7fcf361baf9a77e9b597ac96583c16f79a4fd5
nand chan <nandykins@gmail.com>
2011-11-22 21:25:32 +0100

    removed unused usings, split more files

--- GTL/Types/Adapters.cs
+++ GTL/Types/Adapters.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace GreenTea
 {
@@ -93,59 +91,5 @@ namespace GreenTea
         {
             return '"' + Value + '"';
         }
-    }
-
-    public class GTVoid : Value, GTList
-    {
-        public static readonly GTVoid Void = new GTVoid();
-
-        public override GTType Type
-        {
-            get { return GTType.Void; }
-        }
-
-        public GTVoid() { }
-
-        #region Implementations
-        public int Count
-        {
-            get { return 0; }
-        }
-
-        public GTList Add(Value v)
-        {
-            return new TreeList(v);
-        }
-
-        public IEnumerable<Value> Enumerate()
-        {
-            return new TreeList().Enumerate();
-        }
-
-        public Value this[int i]
-        {
-            get { throw new IndexOutOfRangeException(); }
-        }
-
-        public GTList Set(int i, Value v)
-        {
-            throw new IndexOutOfRangeException();
-        }
-
-        public GTList InsertBefore(int i, Value v)
-        {
-            throw new IndexOutOfRangeException();
-        }
-
-        public GTList InsertAfter(int i, Value v)
-        {
-            throw new IndexOutOfRangeException();
-        }
-
-        public override string ToString()
-        {
-            return "void";
-        }
-        #endregion
     }
 }
