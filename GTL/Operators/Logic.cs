c66d175fd258deb28e339f5335f20670c7c5d09e
nand chan <nandykins@gmail.com>
2011-11-24 23:44:07 +0100

    added missing Logic.cs

---
+++ GTL/Operators/Logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenTea
+{
+    public class AndOp : Operator
+    {
+        public AndOp(IExpression left, IExpression right) : base(left, right) { }
+
+        public override Value Evaluate(Scope scope)
+        {
+            return new GTBool(Left.Evaluate(scope).IsTrue() && Right.Evaluate(scope).IsTrue());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} && {1}", Left, Right);
+        }
+    }
+
+    public class OrOp : Operator
+    {
+        public OrOp(IExpression left, IExpression right) : base(left, right) { }
+
+        public override Value Evaluate(Scope scope)
+        {
+            return new GTBool(Left.Evaluate(scope).IsTrue() || Right.Evaluate(scope).IsTrue());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} || {1}", Left, Right);
+        }
+    }
+}
