085fbecf2652d467bfd8200085f3f8a5ee7550c3
nand chan <nandykins@gmail.com>
2011-11-29 22:19:24 +0100

    actually added new files -.-

---
+++ GTL/Expressions/Scope.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace GreenTea
+{
+    public class ScopeCreation : IExpression
+    {
+        public IExpression Body { get; private set; }
+        public IExpression Parent { get; private set; }
+
+        public ScopeCreation(IExpression body, IExpression parent)
+        {
+            this.Body = body;
+            this.Parent = parent;
+        }
+
+        public Value Evaluate(Scope scope)
+        {
+            Scope s;
+
+            if (Parent == null)
+                s = new Scope(scope, true);
+            else
+                s = new Scope(from v in Parent.Evaluate(scope).Enumerate()
+                              where v.Type == GTType.Scope
+                              select (Scope)v, true);
+
+            Body.Evaluate(s); // return value is ignored
+
+            return s;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("scope {0}{1}", Body, (Parent == null ? "" : String.Format(" has {0}", Parent)));
+        }
+    }
+}
---
+++ GTL/Parsers/Scopes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sprache;
+
+namespace GreenTea
+{
+    public static partial class Parser
+    {
+        private static Parser<IExpression>
+
+        ScopeCreation =
+            from word in Parse.String("scope")
+            from s1 in Parse.WhiteSpace.AtLeastOnce()
+            from expr in Expression
+            from par in ScopeParent.Or(Parse.Return<IExpression>(null))
+            select new ScopeCreation(expr, par),
+
+        ScopeParent =
+            from s1 in Parse.WhiteSpace.AtLeastOnce()
+            from word in Parse.String("has")
+            from s2 in Parse.WhiteSpace.AtLeastOnce()
+            from expr in Expression
+            select expr;
+    }
+}
